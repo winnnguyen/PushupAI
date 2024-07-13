bd0a37f1d2d2ac6e7c1ffe6cd7197e02e3142f59
Winn Nguyen <144104200+winnnguyen@users.noreply.github.com>
2024-07-13 08:29:43 -0700

    Add files via upload

---
+++ script.py
@@ -0,0 +1,46 @@
+import cv2
+import mediapipe as mp 
+import numpy as np 
+import pickle
+import pandas as pd
+
+mp_drawing = mp.solutions.drawing_utils
+mp_pose = mp.solutions.pose
+
+with open('model_pickle', 'rb') as f:
+    model = pickle.load(f)
+
+cap = cv2.VideoCapture(0)
+with mp_pose.Pose(min_detection_confidence = 0.5, min_tracking_confidence = 0.5) as pose:
+    
+    while cap.isOpened():
+        ret, image = cap.read()
+
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        image.flags.writeable = False
+        results = pose.process(image)
+        image.flags.writeable = True
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+        mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS)
+
+        if (results.pose_landmarks):
+            row = np.array([[res.x, res.y, res.z, res.visibility] for res in results.pose_landmarks.landmark]).flatten().reshape(1, -1)
+            body_language_class = model.predict(row)[0]
+            body_language_prob = model.predict_proba(row)[0]
+
+            cv2.rectangle(image, (0,0), (400, 150), (245, 117, 16), -1)
+            cv2.putText(image, 'CLASS', (200,30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2, cv2.LINE_AA)
+            cv2.putText(image, body_language_class.split(' ')[0], (200,102), cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 255, 255), 2, cv2.LINE_AA)
+
+            cv2.putText(image, 'PROB', (20,30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2, cv2.LINE_AA)
+            cv2.putText(image, str(round(body_language_prob[np.argmax(body_language_prob)], 2)), (20,102), cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 255, 255), 2, cv2.LINE_AA)
+
+    
+        cv2.imshow('Raw Webcam Feed', image)
+
+        if cv2.waitKey(10) & 0xFF == ord('q'):
+            break
+
+cap.release()
+cv2.destroyAllWindows()
